e5e95b6b67d2fb50bd270eecee50124c2c714acc
Saito197 <saitohikari197@gmail.com>
2022-10-14 15:46:48 +0700

    Update component utilitities

---
+++ ComponentUtil.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+namespace SaitoGames.Utilities
+{
+    public static class ComponentUtil
+    {
+
+        #region Add new components with values
+        public static T AddComponentExt<T, T1>(this GameObject go, T1 arg1) where T : ExtendedBehaviour<T1>
+        {
+            if (go == null)
+                return null;
+            
+            var newComponent = go.AddComponent<T>();
+            newComponent.OnCreate(arg1);
+            return newComponent;
+        }
+        
+        public static T AddComponentExt<T, T1, T2>(this GameObject go, T1 arg1, T2 arg2) where T : ExtendedBehaviour<T1, T2>
+        {
+            if (go == null)
+                return null;
+
+            var newComponent = go.AddComponent<T>();
+            newComponent.OnCreate(arg1, arg2);
+            return newComponent;
+        }
+        
+        public static T AddComponentExt<T, T1, T2, T3>(this GameObject go, T1 arg1, T2 arg2, T3 arg3) where T : ExtendedBehaviour<T1, T2, T3>
+        {
+            if (go == null)
+                return null;
+
+            var newComponent = go.AddComponent<T>();
+            newComponent.OnCreate(arg1, arg2, arg3);
+            return newComponent;
+        }
+        
+        public static T AddComponentExt<T, T1, T2, T3, T4>(this GameObject go, T1 arg1, T2 arg2, T3 arg3, T4 arg4) where T : ExtendedBehaviour<T1, T2, T3, T4>
+        {
+            if (go == null)
+                return null;
+
+            var newComponent = go.AddComponent<T>();
+            newComponent.OnCreate(arg1, arg2, arg3, arg4);
+            return newComponent;
+        }
+
+        #endregion
+
+        #region GameObject Instantiation 
+        public static T InstantiateExt<T>(T prefab) where T : Component
+        {
+            var go = Object.Instantiate(prefab);
+            var extBehaviours = go.GetComponents<ExtendedBehaviour>();
+            foreach (var component in extBehaviours)
+            {
+                component.OnCreate();
+            }
+            return go;
+        }
+        
+        public static T InstantiateExt<T>(T prefab, Transform parent) where T : Component
+        {
+            var go = Object.Instantiate(prefab, parent);
+            var extBehaviours = go.GetComponents<ExtendedBehaviour>();
+            foreach (var component in extBehaviours)
+            {
+                component.OnCreate();
+            }
+            return go;
+        }
+        
+        public static T InstantiateExt<T>(T prefab, Vector3 position, Quaternion rotation, Transform parent) where T : Component
+        {
+            var go = Object.Instantiate(prefab, position, rotation, parent);
+            var extBehaviours = go.GetComponents<ExtendedBehaviour>();
+            foreach (var component in extBehaviours)
+            {
+                component.OnCreate();
+            }
+            return go;
+        }
+
+        #endregion
+    }
+}
---
+++ ExtendedBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+namespace SaitoGames.Utilities
+{
+    public abstract class ExtendedBehaviour : MonoBehaviour
+    {
+        public virtual void OnCreate() { }
+    }
+
+    public abstract class ExtendedBehaviour<T1> : ExtendedBehaviour
+    {
+        [SerializeField] private T1 Value1;
+        public abstract void OnCreate(T1 arg1);
+
+        public override void OnCreate() 
+        {
+            if (Value1 == null)
+                return;
+
+            OnCreate(Value1); 
+        }
+    }
+
+    public abstract class ExtendedBehaviour<T1, T2> : ExtendedBehaviour
+    {
+        [SerializeField] private T1 Value1;
+        [SerializeField] private T2 Value2;
+        public abstract void OnCreate(T1 arg1, T2 arg2);
+        public override void OnCreate()
+        {
+            if (Value1 == null || Value2 == null)
+                return;
+
+            OnCreate(Value1, Value2);
+        }
+    }
+
+    public abstract class ExtendedBehaviour<T1, T2, T3> : ExtendedBehaviour
+    {
+        [SerializeField] private T1 Value1;
+        [SerializeField] private T2 Value2;
+        [SerializeField] private T3 Value3;
+        public abstract void OnCreate(T1 arg1, T2 arg2, T3 arg3);
+        public override void OnCreate()
+        {
+            if (Value1 == null || Value2 == null || Value3 == null)
+                return;
+
+            OnCreate(Value1, Value2, Value3);
+        }
+    }
+
+    public abstract class ExtendedBehaviour<T1, T2, T3, T4> : ExtendedBehaviour
+    {
+        [SerializeField] private T1 Value1;
+        [SerializeField] private T2 Value2;
+        [SerializeField] private T3 Value3;
+        [SerializeField] private T4 Value4;
+
+        public abstract void OnCreate(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+        public override void OnCreate()
+        {
+            if (Value1 == null || Value2 == null || Value3 == null || Value4 == null)
+                return;
+
+            OnCreate(Value1, Value2, Value3, Value4);
+        }
+    }
+
+
+}
