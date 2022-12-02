8b3cbfee8aefb259f3dcfe54cae8b22c092142f5
Saito197 <saitohikari197@gmail.com>
2022-12-02 19:16:47 +0700

    Reworking ExtendedBehaviour OnCreate method

--- ExtendedBehaviour.cs
+++ ExtendedBehaviour.cs
@@ -10,23 +10,31 @@ namespace SaitoGames.Utilities
 
     public abstract class ExtendedBehaviour<T1> : ExtendedBehaviour
     {
-        [SerializeField] private T1 Value1;
-        public abstract void OnCreate(T1 arg1);
+        [SerializeField] protected T1 Value1;
+        public virtual void OnCreate(T1 arg1)
+        {
+            Value1 = arg1;
+        }
 
-        public override void OnCreate() 
+        public override void OnCreate()
         {
             if (Value1 == null)
                 return;
 
-            OnCreate(Value1); 
+            OnCreate(Value1);
         }
     }
 
     public abstract class ExtendedBehaviour<T1, T2> : ExtendedBehaviour
     {
-        [SerializeField] private T1 Value1;
-        [SerializeField] private T2 Value2;
-        public abstract void OnCreate(T1 arg1, T2 arg2);
+        [SerializeField] protected T1 Value1;
+        [SerializeField] protected T2 Value2;
+        public virtual void OnCreate(T1 arg1, T2 arg2)
+        {
+            Value1 = arg1;
+            Value2 = arg2;
+        }
+
         public override void OnCreate()
         {
             if (Value1 == null || Value2 == null)
@@ -38,10 +46,16 @@ namespace SaitoGames.Utilities
 
     public abstract class ExtendedBehaviour<T1, T2, T3> : ExtendedBehaviour
     {
-        [SerializeField] private T1 Value1;
-        [SerializeField] private T2 Value2;
-        [SerializeField] private T3 Value3;
-        public abstract void OnCreate(T1 arg1, T2 arg2, T3 arg3);
+        [SerializeField] protected T1 Value1;
+        [SerializeField] protected T2 Value2;
+        [SerializeField] protected T3 Value3;
+        public virtual void OnCreate(T1 arg1, T2 arg2, T3 arg3)
+        {
+            Value1 = arg1;
+            Value2 = arg2;
+            Value3 = arg3;
+        }
+
         public override void OnCreate()
         {
             if (Value1 == null || Value2 == null || Value3 == null)
@@ -53,12 +67,19 @@ namespace SaitoGames.Utilities
 
     public abstract class ExtendedBehaviour<T1, T2, T3, T4> : ExtendedBehaviour
     {
-        [SerializeField] private T1 Value1;
-        [SerializeField] private T2 Value2;
-        [SerializeField] private T3 Value3;
-        [SerializeField] private T4 Value4;
+        [SerializeField] protected T1 Value1;
+        [SerializeField] protected T2 Value2;
+        [SerializeField] protected T3 Value3;
+        [SerializeField] protected T4 Value4;
 
-        public abstract void OnCreate(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+        public virtual void OnCreate(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            Value1 = arg1;
+            Value2 = arg2;
+            Value3 = arg3;
+            Value4 = arg4;
+        }
+
         public override void OnCreate()
         {
             if (Value1 == null || Value2 == null || Value3 == null || Value4 == null)
