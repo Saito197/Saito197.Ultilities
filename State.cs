11892906344badf121bfac3a24ec3b4c463bd536
Saito <saitohikari197@gmail.com>
2023-09-19 17:10:03 +0700

    Adding new state machine abstracts

---
+++ State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace SaitoGames.SmasherGame.Character
+{
+    public delegate void StateChangeEventHandler(Type newState);
+
+    public abstract class State 
+    {
+        public event StateChangeEventHandler StateChange;
+
+        private StateMachine _stateMachine;
+
+        public State(StateMachine stateMachine)
+        { 
+            _stateMachine = stateMachine;
+        }
+
+        public abstract void StateUpdate();
+        public abstract void ActionCommand(CharacterAction action);
+        public abstract void DirectionCommand(Vector2 direction);
+    }
+}
