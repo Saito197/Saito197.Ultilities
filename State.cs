75ed972b30fd7996b142e167aacf26881689284a
Saito <saitohikari197@gmail.com>
2023-09-19 17:36:03 +0700

    Resolving state dependencies problems

--- State.cs
+++ State.cs
@@ -6,6 +6,10 @@ namespace SaitoGames.Utilities
     public enum CharacterAction
     {
         // Define a list of input action/commands 
+        Dodge, 
+        Attack,
+        Special1,
+        Special2
     }
 
     public delegate void StateChangeEventHandler(Type newState);
@@ -16,12 +20,18 @@ namespace SaitoGames.Utilities
 
         private StateMachine _stateMachine;
 
+        // Base Constructor to get reference to the main StateMachine
+        // Define a different constructor to pass in extra parameters when inheriting the state class.
         public State(StateMachine stateMachine)
         { 
             _stateMachine = stateMachine;
         }
 
+        public void OnStateEnter() { }
+        public void OnStateExit() { }
         public abstract void StateUpdate();
+
+
         public abstract void ActionCommand(CharacterAction action);
         public abstract void DirectionCommand(Vector2 direction);
     }
